23e65525e52ab5fae66698ef44d89341e85b29dd
shhumen <mehtiyevashumen@gmail.com>
2024-01-19 13:28:08 +0400

    card-game

---
+++ memory_card_game/index.js
@@ -0,0 +1,104 @@
+const gridContainer = document.querySelector('.grid-container')
+
+let cards = []
+let firstCard, secondCard
+let lockBoard = false
+let score = 0
+
+document.querySelector('.score').textContent = score
+
+fetch('./data/data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data]
+    shuffleCards()
+    generateCards()
+  })
+
+function shuffleCards() {
+  let currentIndex = cards.length
+  let randomIndex
+  let tempValue
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    tempValue = cards[currentIndex]
+    cards[currentIndex] = cards[randomIndex]
+    cards[randomIndex] = tempValue
+  }
+}
+
+function generateCards() {
+  for (let card of cards) {
+    const cardElement = document.createElement('div')
+    cardElement.classList.add('card')
+    cardElement.setAttribute('data-name', card.name)
+    cardElement.innerHTML = `
+    <div class="front"><img class="front-image" src=${card.image} alt=${card.name}></div>
+    <div class="back">
+      <img class="back-image" src="./assets/main.png" alt="back" />
+    </div>
+    `
+    gridContainer.appendChild(cardElement)
+    cardElement.addEventListener('click', flipCard)
+  }
+}
+
+function resetBoard() {
+  firstCard = null
+  secondCard = null
+  lockBoard = false
+}
+
+function disableCard() {
+  setTimeout(() => {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+    firstCard.remove()
+    secondCard.remove()
+    resetBoard()
+  }, 1000)
+}
+
+function unFlipCard() {
+  setTimeout(() => {
+    firstCard.classList.remove('flipped')
+    secondCard.classList.remove('flipped')
+    resetBoard()
+  }, 1500)
+}
+
+function flipCard() {
+  if (lockBoard) return
+  if (this === firstCard) return
+
+  this.classList.add('flipped')
+  if (!firstCard) {
+    firstCard = this
+    return
+  }
+
+  secondCard = this
+  lockBoard = true
+
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name
+
+  if (isMatch) {
+    // score += 10
+    document.querySelector('.score').textContent = score
+    disableCard()
+  } else {
+    // score -= 10
+    document.querySelector('.score').textContent = score
+    unFlipCard()
+  }
+}
+
+function restart() {
+  resetBoard()
+  shuffleCards()
+  score = 0
+  document.querySelector('.score').textContent = score
+  gridContainer.innerHTML = ''
+  generateCards()
+}
